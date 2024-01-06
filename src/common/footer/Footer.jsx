e265aa188b61a5722a3a01c15ccc01927e75ccd3
fadeyibigbolahan <fadeyibi26@gmail.com>
2024-01-06 15:27:12 +0100

    Initial commit

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -1,27 +1,27 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <div className='container grid2'>
-          <div className='box'>
+        <div className="container grid2">
+          <div className="box">
             <h1>Bonik</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
-            <div className='icon d_flex'>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-google-play'></i>
+            <p>Leading E-Commerce site in the world.</p>
+            <div className="icon d_flex">
+              <div className="img d_flex">
+                <i class="fa-brands fa-google-play"></i>
                 <span>Google Play</span>
               </div>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-app-store-ios'></i>
+              <div className="img d_flex">
+                <i class="fa-brands fa-app-store-ios"></i>
                 <span>App Store</span>
               </div>
             </div>
           </div>
 
-          <div className='box'>
+          <div className="box">
             <h2>About Us</h2>
             <ul>
               <li>Careers</li>
@@ -31,7 +31,7 @@ const Footer = () => {
               <li>Privacy Policy</li>
             </ul>
           </div>
-          <div className='box'>
+          <div className="box">
             <h2>Customer Care</h2>
             <ul>
               <li>Help Center </li>
@@ -41,18 +41,18 @@ const Footer = () => {
               <li>Returns & Refunds </li>
             </ul>
           </div>
-          <div className='box'>
+          <div className="box">
             <h2>Contact Us</h2>
             <ul>
-              <li>70 Washington Square South, New York, NY 10012, United States </li>
-              <li>Email: uilib.help@gmail.com</li>
-              <li>Phone: +1 1123 456 780</li>
+              <li>Ikeja, Lagos State, Nigeria </li>
+              <li>Email: support@kingwaretech.com</li>
+              <li>Phone: 09038614081</li>
             </ul>
           </div>
         </div>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
